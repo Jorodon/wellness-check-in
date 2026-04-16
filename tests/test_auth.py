2a83adeeff51561f8f9cd176ef36e18351114253
Jordon <jordontaylorjpt@gmail.com>
2026-04-16 12:37:51 -0400

    Fixed issues with authentication pytests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,10 +1,13 @@
 from checkin_backend.app.models import User
 
+# ------- This file contains tests for the authentication functionality of the wellness check-in application. ------- #
+
 # Password too short test case
 def test_login_01(client):
     response = client.post(
         "/auth/register",
         json={
+            "username": "test",
             "email": "student@ufl.edu",
             "password1": "123",
             "password2": "123"
@@ -14,9 +17,10 @@ def test_login_01(client):
     assert response.status_code == 400
     assert response.get_json()["error"] == "Password has to be at least 8 characters"
 
-#Successful registration test case
+# Successful registration test case
 def test_login_02(client, app):
     response = client.post("/auth/register", json={
+        "username": "test",
         "email": "wellnessRules@ufl.edu",
         "password1": "GatorCheck1",
         "password2": "GatorCheck1"
@@ -26,7 +30,7 @@ def test_login_02(client, app):
     assert response.get_json()["message"] == "User registered successfully"
 
     with app.app_context():
-        user = User.query.filter_by(email="wellnessRules@ufl.edu").first()
+        user = User.query.filter_by(email="wellnessRules@ufl.edu".lower()).first()
         assert user is not None
 
 
@@ -48,6 +52,7 @@ def test_login_04(client, test_user):
     response = client.post(
         "/auth/register",
         json={
+            "username": "test",
             "email": "test@example.com",
             "password1": "password123",
             "password2": "password123"
@@ -90,6 +95,7 @@ def test_login_07(client):
     response = client.post(
         "/auth/register",
         json={
+            "username": "test",
             "email": "mismatch@ufl.edu",
             "password1": "password123",
             "password2": "different123"
